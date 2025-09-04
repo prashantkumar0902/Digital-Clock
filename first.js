78f0fe3b0a611f8a49fdaacaf5384e26b1078ca3
prashantkumar0902 <shlukumar2003@gmail.com>
2025-09-04 21:49:18 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,28 @@
+ function updateClock() {
+      let now = new Date();
+
+      // Time
+      let hours = now.getHours();
+      let minutes = now.getMinutes();
+      let seconds = now.getSeconds();
+      let ampm = hours >= 12 ? "PM" : "AM";
+
+      hours = hours % 12;
+      hours = hours ? hours : 12; // 0 ko 12 banane ke liye
+
+      hours = String(hours).padStart(2, "0");
+      minutes = String(minutes).padStart(2, "0");
+      seconds = String(seconds).padStart(2, "0");
+
+      document.getElementById("clock").innerText =
+        `${hours}:${minutes}:${seconds} ${ampm}`;
+
+      // Date
+      let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+      let todayDate = now.toLocaleDateString("en-US", options);
+
+      document.getElementById("date").innerText = todayDate;
+    }
+
+    setInterval(updateClock, 1000);
+    updateClock(); 
